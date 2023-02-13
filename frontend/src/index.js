e8bc40652b9bc8d8d5afa4512e6a910f8bc8d31b
Maciej Jaworski <mcjjaworski@gmail.com>
2023-02-13 18:12:18 +0100

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import store from "./store";
+import { Provider } from "react-redux";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
+
+root.render(<App />);
